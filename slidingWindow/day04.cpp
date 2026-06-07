0974320f8138f6b995066410f40ae05ef6f82153
Shivansh Saxena <shivanshsaxenagwl6@gmail.com>
2026-06-07 23:33:02 +0530

    Add comments to clarify sliding window algorithm steps

--- slidingWindow/day04.cpp
+++ slidingWindow/day04.cpp
@@ -1,3 +1,12 @@
+// 1.) We have to initialize a result. vector.
+// 2.) We have to intialize a left pointer. 
+// 3.) We have to initialize. a Map 
+// 4.) We have to run for loop with right till the size of an array.
+// 5.) We will insert the element in map and increment.
+// 6.) if window > k map[left--] if left frequency in. map ===0 then we will erase it from the map
+// 7.) if window == k then. result.push_back(map.size()).
+
+
 #include<iostream>
 #include<vector>
 #include<unordered_map>
@@ -7,9 +16,12 @@ using namespace std;
 
 
 int main(){    
-
+    
+    
     vector<int>arr = {1,2,1,3,4,2,3};  
     int k =4;
+
+    
     int left = 0;
     vector<int>ans; 
     unordered_map<int,int>mp; 
