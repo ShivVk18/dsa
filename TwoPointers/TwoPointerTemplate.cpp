b55ccfc4a8ca73ea730e41c168d6bc1cb94e7fc0
Shivansh Saxena <shivanshsaxenagwl6@gmail.com>
2026-06-22 23:37:15 +0530

    Add initial Two Pointer template with comments for guidance

---
+++ TwoPointers/TwoPointerTemplate.cpp
@@ -0,0 +1,9 @@
+// while(left<=right){
+     
+//     we have to right some condition based on the given question ki when do we move left and right pointer 
+
+
+//     compute the ans inside the while
+
+
+// } 
