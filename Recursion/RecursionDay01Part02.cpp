4ea795701e59772907261687061cf1554166c515
Shivansh Saxena <shivanshsaxenagwl6@gmail.com>
2026-07-10 22:49:58 +0530

    Implement structural updates and optimizations across multiple modules

---
+++ Recursion/RecursionDay01Part02.cpp
@@ -0,0 +1,12 @@
+//fibonacci series
+
+
+    //    if(n<=1){
+    //        return n;
+    //    }
+
+
+       
+
+
+    // return generateFibonacciNumbers(n-2) + generateFibonacciNumbers(n-1);
--- Recursion/RecusrionDay01.cpp
+++ Recursion/RecusrionDay01.cpp
@@ -1,12 +1,28 @@
+
+
+//Factorial of a number 
+
+
 #include<iostream>
 using namespace std;
 
+int solve(int n){
+          if(n == 0 || n==1){
+            return 1;
+          } 
+
+
+          return n * solve(n-1);
+
+
+    }
+
 int main(){ 
 
-    
-    
+     
+  int ans = solve(5);
 
-
+cout<<ans;
 
     return 0;                          
 }
